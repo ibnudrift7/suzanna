38c130216c64eeae21aa16a780c1c944ee9b30fd
Pandu Bagoes Pradana <pandubaguspr@gmail.com>
2019-08-06 12:11:53 +0700

    Home, About-Backgrd, About-Visimisi, Product-exclusive, Product-support

---
+++ protected/views/home/about-visimisi.php
@@ -0,0 +1,111 @@
+<section class="about-visimisi-sec-1">
+    <div class="prelative container">
+        <div class="row">
+            <div class="col-md-60">
+                <div class="boxed-tittle-visimisi">
+                    <div class="inner">
+                        <h3>It has been our culture of caring and loving that motivates all our actions, but our corporate focus is guided by its Vision and Mission which direct the business operational that we perform every day on behalf of our country and its residents.
+                            </h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-30">
+                <h3>Vision</h3>
+            </div>
+            <div class="col-md-30">
+                <ul>
+                    <li>Keeping our minds open for creative discoveries and innovations, regarding to updated products and service methods to our customers</li>
+                    <li>Trying the best to provide the complete and good quality products with excellent services and hospitality, in tidy and cozy shopping environment for our customers</li>
+                    <li>Consistently creating a growth number of loyal customers, created by our overall store’s quality that makes our stores different and better compared to our competitors</li>
+                </ul>
+            </div>
+            <div class="col-md-30">
+                <h3>Mision</h3>
+            </div>
+            <div class="col-md-30">
+                <ul>
+                    <li>Creating a clean, pleasantly tidy and cozy shopping environment for the customers</li>
+                    <li>Trying the best to make customers coming back to our stores because of overall store’s quality that makes us different</li>
+                    <li>Ensurinng updated availabilities, especially the latest trends on fashion and toys</li>
+                </ul>
+            </div>
+            <div class="col-md-60">
+                <div class="img"></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="home-sec-4">
+        <div class="prelative container">
+            <div class="row">
+                <div class="col-md-60">
+                        <div class="img"></div>
+                    <div class="subtitle">
+                        <h3>Get In Touch With Suzanna Babyshop</h3>
+                    </div>
+                    <div class="caption">
+                        <p>Our resourceful team will get in touch with you, please leave your contact detail below.</p>
+                    </div>
+                </div>
+                <div class="col-md-15">
+                    <label for="Your Name">Your Name</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="col-md-15">
+                    <label for="Your Mail">Your Mail</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="col-md-15">
+                    <label for="Your Mobile Phone">Your Mobile Phone</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="col-md-15">
+                    <button class="submit">Submit</button>
+                </div>
+            </div>
+    </section>
+    
+    <section class="home-sec-5">
+        <div class="prelative container">
+            <div class="row">
+                <div class="col-md-20">
+                    <div class="img"></div>
+                    <h6>Whatsapp Hotline</h6>
+                    <p>081 650 3636 (Click to chat)</p>
+                </div>
+                <div class="col-md-20">
+                    <div class="img"></div>
+                    <h6>Email</h6>
+                    <p>info@suzannababyshop</p>
+                </div>
+                <div class="col-md-20">
+                    <div class="img"></div>
+                    <h6>Instagram</h6>
+                    <p>suzannababyshop</p>
+                </div>
+                <div class="col-md-20">
+                    <div class="img"></div>
+                    <h6>Facebook</h6>
+                    <p>suzannababyshop</p>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+    
+    <section class="footer">
+        <div class="prelative container">
+            <div class="row">
+                <div class="col-md-20">
+                    <div class="img"></div>
+                </div>
+                <div class="col-md-20">
+                    <p>Copyright © 2019 - Suzanna Babyshop. </br> All rights reserved.</p>
+                </div>
+                <div class="col-md-20">
+                    <p>Website design by Mark Design</p>
+                </div>
+            </div>
+        </div>
+    </section>
---
+++ protected/views/home/product-support.php
@@ -0,0 +1,367 @@
+<section class="product-support-sec-1">
+    <div class="prelative container">
+        <div class="row">
+            <div class="col-md-60">
+                <div class="boxed">
+                    <div class="inner">
+                        <h3>Suzanna Baby Shop’s supporting brands are a qualified and favorite products we proud to offer to our customers.</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-60">
+                <ul>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-60">
+                <ul>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-60">
+                <ul>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-60">
+                <ul>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-60">
+                <ul>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-60">
+                <ul>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-60">
+                <ul>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-60">
+                <ul>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-60">
+                <ul>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-60">
+                <ul>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                    <li>
+                        <div class="img"></div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="home-sec-4">
+        <div class="prelative container">
+            <div class="row">
+                <div class="col-md-60">
+                    <div class="img"></div>
+                    <div class="subtitle">
+                        <h3>Get In Touch With Suzanna Babyshop</h3>
+                    </div>
+                    <div class="caption">
+                        <p>Our resourceful team will get in touch with you, please leave your contact detail below.</p>
+                    </div>
+                </div>
+                <div class="col-md-15">
+                    <label for="Your Name">Your Name</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="col-md-15">
+                    <label for="Your Mail">Your Mail</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="col-md-15">
+                    <label for="Your Mobile Phone">Your Mobile Phone</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="col-md-15">
+                    <button class="submit">Submit</button>
+                </div>
+            </div>
+    </section>
+    
+    <section class="home-sec-5">
+        <div class="prelative container">
+            <div class="row">
+                <div class="col-md-20">
+                    <div class="img"></div>
+                    <h6>Whatsapp Hotline</h6>
+                    <p>081 650 3636 (Click to chat)</p>
+                </div>
+                <div class="col-md-20">
+                    <div class="img"></div>
+                    <h6>Email</h6>
+                    <p>info@suzannababyshop</p>
+                </div>
+                <div class="col-md-20">
+                    <div class="img"></div>
+                    <h6>Instagram</h6>
+                    <p>suzannababyshop</p>
+                </div>
+                <div class="col-md-20">
+                    <div class="img"></div>
+                    <h6>Facebook</h6>
+                    <p>suzannababyshop</p>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+    
+    <section class="footer">
+        <div class="prelative container">
+            <div class="row">
+                <div class="col-md-20">
+                    <div class="img"></div>
+                </div>
+                <div class="col-md-20">
+                    <p>Copyright © 2019 - Suzanna Babyshop. </br> All rights reserved.</p>
+                </div>
+                <div class="col-md-20">
+                    <p>Website design by Mark Design</p>
+                </div>
+            </div>
+        </div>
+    </section>
